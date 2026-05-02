df95308b326a4d5098e56b83ea0ac788facddf86
Hakan Gunes <hakangunes80@gmail.com>
2026-05-02 10:28:32 +0200

    init 5

--- src/main/java/postgres/PgCore.java
+++ src/main/java/postgres/PgCore.java
@@ -1,4 +1,14 @@
 package postgres;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class PgCore {
+
+
+    private static final Logger log = LoggerFactory.getLogger(PgCore.class);
+
+    public static void get() {
+        log.info("test started");
+    }
 }
--- src/test/java/postgres/PgCoreTest.java
+++ src/test/java/postgres/PgCoreTest.java
@@ -1,7 +1,13 @@
 package postgres;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
 
 class PgCoreTest {
+
+
+    @Test
+    void test_given_jdbc_client_should_establish_connection_with_postgres() {
+        PgCore.get();
+    }
 
 }
