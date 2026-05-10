78782f088a487f343694254b4ed4d0172dafa2dc
Hakan Gunes <hakangunes80@gmail.com>
2026-05-10 12:57:18 +0200

    start spark cassandra

--- src/main/java/infrastructure/spark/SparkCassandra.java
+++ src/main/java/infrastructure/spark/SparkCassandra.java
@@ -1,4 +1,33 @@
 package infrastructure.spark;
 
+import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.Row;
+import org.apache.spark.sql.SaveMode;
+
 public class SparkCassandra {
+    // https://github.com/apache/cassandra-spark-connector/blob/trunk/doc/14_data_frames.md
+    // https://github.com/apache/cassandra-spark-connector/blob/trunk/doc/reference.md
+
+    public record Info(
+            Dataset<Row> df,
+            String keyspace,
+            String table,
+            String cql,
+            long durationTimeMs,
+            long triggerTimeMs) {
+    }
+
+    public Info write(Dataset<Row> dataset, String url, String user, String pass,
+                      String keyspace, String table, SaveMode saveMode) {
+
+        dataset.write()
+                .format("cassandra")
+                .option("keyspace", keyspace)
+                .option("table", table)
+                .mode(saveMode)
+                .save();
+
+        return new Info(null, keyspace, table, null, 0L, 0L);
+    }
+
 }
